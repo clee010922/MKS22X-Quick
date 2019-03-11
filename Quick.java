45d154f956a4ae38e50877c3250c669046b1edb6
clee010922 <clee01@stuy.edu>
2019-03-11 10:09:40 -0400

    added base code

---
+++ Quick.java
@@ -0,0 +1,26 @@
+public class Quick {
+
+  /*
+  Choose a random pivot element between the start and end index inclusive,
+  Then modify the array such that:
+  *1. Only the indices from start to end inclusive are considered in range
+  *2. A random index from start to end inclusive is chosen, the corresponding
+  *   element is designated the pivot element.
+  *3. all elements in range that are smaller than the pivot element are placed before the pivot element.
+  *4. all elements in range that are larger than the pivot element are placed after the pivot element.
+  *@return the index of the final position of the pivot element.
+  */
+  public static int partition (int[] data, int start, int end){
+
+
+
+ }
+
+ /*return the value that is the kth smallest value of the array.
+ */
+ public static int quickselect(int []data, int k){
+
+ }
+
+
+}
