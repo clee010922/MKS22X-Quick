1dc938ec44fc6f53e9f7a7dfa7b6877d9dc56aaf
Chong Lee <clee010922@gmail.com>
2019-03-11 23:50:24 -0400

    completed partition and used the driver to test it

--- Quick.java
+++ Quick.java
@@ -10,20 +10,45 @@ public class Quick {
   *4. all elements in range that are larger than the pivot element are placed after the pivot element.
   *@return the index of the final position of the pivot element.
   */
+
   public static int partition (int[] data, int start, int end){
     Random random = new Random();
     int index = random.nextInt(end-start+1) + start;
     int pivot = data[index];
-    for (int i = start; i < end; i++) {
-      if (data[i] < pivot) {
-
+    int temp = start;
+    swap(index, end, data);
+    index = end;
+    end--;
+    while (start <= end) {
+      if (data[start] == pivot) {
+        start++;
+      }
+      else {
+        if (data[start] < pivot) {
+          swap(start, temp, data);
+          start++;
+          temp++;
+        }
+        else {
+          swap(start, end, data);
+          end--;
+        }
       }
     }
+    swap(index, start, data);
+    index = start;
+    return index;
   }
 
+  public static void swap(int index1, int index2, int[] data) {
+    int temp = data[index1];
+    data[index1] = data[index2];
+    data[index2] = temp;
+  }
+  /*
   //return the value that is the kth smallest value of the array.
   public static int quickselect(int[] data, int k) {
 
   }
-
+  */
 }
