16adaeb18737f4e9248df1fd0d4c621910a1109b
Chong Lee <clee010922@gmail.com>
2019-03-12 00:11:26 -0400

    finished quickselect. tested with driver2

--- Quick.java
+++ Quick.java
@@ -1,15 +1,5 @@
 import java.util.*;
 public class Quick {
-  /*
-  Choose a random pivot element between the start and end index inclusive,
-  Then modify the array such that:
-  *1. Only the indices from start to end inclusive are considered in range
-  *2. A random index from start to end inclusive is chosen, the corresponding
-  *   element is designated the pivot element.
-  *3. all elements in range that are smaller than the pivot element are placed before the pivot element.
-  *4. all elements in range that are larger than the pivot element are placed after the pivot element.
-  *@return the index of the final position of the pivot element.
-  */
 
   public static int partition (int[] data, int start, int end){
     Random random = new Random();
@@ -45,10 +35,21 @@ public class Quick {
     data[index1] = data[index2];
     data[index2] = temp;
   }
-  /*
+
   //return the value that is the kth smallest value of the array.
   public static int quickselect(int[] data, int k) {
-
+    int start = 0;
+    int end = data.length - 1;
+    for (int i = 0; i < data.length; i++) {
+      int index = partition(data, start, end);
+      if (index > k)
+        end = index;
+      if (index < k)
+        start = index;
+      if (index == k)
+        return data[k];
+    }
+    return data[k];
   }
-  */
+  
 }
