1207d8eaa2f796590581700910b780478a484864
clee010922 <clee01@stuy.edu>
2019-03-14 10:19:30 -0400

    added mr. K's drvier for quicksort and added a wrapper function for quick sort

--- Quick.java
+++ Quick.java
@@ -110,6 +110,7 @@ public class Quick {
     return result;
   }
 
+  /*
   public static void main (String[] args) {
     int[] quick = {100, 1519438594, -2342342, 4, 4, 3, 3, 2, 2, 1, 1};
     //int i = Quick.partition(quick, 0, 12);
@@ -118,6 +119,12 @@ public class Quick {
     quicksort(quick, 0, 10);
     System.out.println(printArray(quick));
   }
+  */
+
+  public static void quicksort(int[] data) {
+    quicksort(data, 0, data.length-1);
+  }
+
 
   public static void quicksort(int[] data, int lo, int hi) {
     if (lo < hi) {
@@ -127,6 +134,39 @@ public class Quick {
     }
   }
 
-
+  public static void main(String[]args){
+    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+    int[]MAX_LIST = {1000000000,500,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=2){
+        long qtime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=5; trial++){
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          Quick.quicksort(data2);
+          t2 = System.currentTimeMillis();
+          qtime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+          }
+        }
+        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+      }
+      System.out.println();
+    }
+  }
 
 }
